b25ebb1b0430805da9f51e975477bbc91c6f2c6f
Rayvid <gin.kisonas@gmail.com>
2020-04-09 00:06:10 +0300

    Initial commit of stress test app skeleton and so DI implemented

---
+++ StressTest/StressTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StressTest
+{
+    class ConsoleHost
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("usage: StressTest.exe local/remote #");
+                return;
+            }
+            else
+            {
+                var serviceProvide = new ServiceCollection()
+                    .AddLogging((logging) =>
+                    {
+                        logging.ClearProviders();
+                        // Standard way to configure logging, can ommit in this app since we are not using any configuration sources
+                        // logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                        logging.AddConsole();
+                        logging.SetMinimumLevel(LogLevel.Debug);
+                        // TODO our own file logging provider
+                    }).BuildServiceProvider();
+
+                var logger = serviceProvide.GetService<ILogger<ConsoleHost>>();
+
+                logger.LogInformation("Starting stress test app");
+                Thread.Sleep(10000);
+                // TODO further logic
+            }
+        }
+    }
+}
